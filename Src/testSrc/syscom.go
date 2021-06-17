5b4dd0644d08e3df3bebd1de7df2d5a0373b28a2
dayeon <dayeon2768@daum.net>
2021-06-18 03:21:26 +0900

    fix

--- Src/testSrc/syscom.go
+++ Src/testSrc/syscom.go
@@ -3,12 +3,12 @@ package main
 import (
 "os/exec"
 "github.com/hyperledger/fabric-chaincode-go/shim"
-pb "github.com/hyperledger/fabric-protos-go/peer"
+"github.com/hyperledger/fabric-protos-go/peer"
 )
 
 type SimpleChaincode struct {}
 
-func (t *SimpleChaincode) example(stub shim.ChaincodeStubInterface, key string) pb.Response {
+func (t *SimpleChaincode) example(stub shim.ChaincodeStubInterface, key string) peer.Response {
 	out, err:= exec.Command("date").Output()
 	if err != nil {
 	shim.Error("error")
